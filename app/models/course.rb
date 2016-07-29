5861951d02d6a577ea098746de7e8da7e6ea5d27
rajesh silambarasan <silambarasan.kj@gmail.com>
2016-07-29 19:40:46 +0530

    paper clip and heroku updates

--- app/models/course.rb
+++ app/models/course.rb
@@ -2,5 +2,5 @@ class Course < ActiveRecord::Base
   belongs_to :user
 has_attached_file :image, styles: { small: "64x64", med: "100x100", large: "200x200" }
 validates :image, :attachment_presence => true
-validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
+validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"] #validate mandatory or specifiy no validates
 end
