d4e3583ecd14e53743111082ddb087c9d6f7c7c1
rajesh silambarasan <silambarasan.kj@gmail.com>
2016-07-29 18:54:27 +0530

    paper clip gem working stage

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -72,6 +72,6 @@ load_and_authorize_resource
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def course_params
-      params.require(:course).permit(:name, :description, :price, :user_id)
+      params.require(:course).permit(:name, :description, :price, :user_id, :image)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160728135901) do
+ActiveRecord::Schema.define(version: 20160729115735) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string   "namespace"
