3f16ad50e1ae10e40b9d21213419ad5508309d18
rajesh silambarasan <silambarasan.kj@gmail.com>
2016-07-30 15:04:52 +0530

    sqlite move to development1

--- db/schema.rb
+++ db/schema.rb
@@ -64,14 +64,10 @@ ActiveRecord::Schema.define(version: 20160729115735) do
   create_table "courses", force: :cascade do |t|
     t.string   "name"
     t.text     "description"
-    t.decimal  "price",              precision: 5, scale: 2
+    t.decimal  "price",       precision: 5, scale: 2
     t.integer  "user_id"
-    t.datetime "created_at",                                 null: false
-    t.datetime "updated_at",                                 null: false
-    t.string   "image_file_name"
-    t.string   "image_content_type"
-    t.integer  "image_file_size"
-    t.datetime "image_updated_at"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
   end
 
   add_index "courses", ["user_id"], name: "index_courses_on_user_id"
